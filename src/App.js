b75549610ed866c64f08e991c70bb49aca38fdbc
iftahprogramming <iftahprogramming@gmail.com>
2022-08-29 21:01:35 +0300

    making expense list dynamic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import react, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+const intialExpenses = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 295,
+    date: new Date(2021, 2, 28),
+  },
+  { id: "e2", title: "BBB", amount: 60, date: new Date(2021, 2, 28) },
+  { id: "e3", title: "AC", amount: 40, date: new Date(2021, 2, 28) },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 295,
-      date: new Date(2021, 2, 28),
-    },
-    { id: "e2", title: "BBB", amount: 60, date: new Date(2021, 2, 28) },
-    { id: "e3", title: "AC", amount: 40, date: new Date(2021, 2, 28) },
-  ];
+  const [expenses, setExpenses] = useState(intialExpenses);
+
   const addExpenseHandler = (expense) => {
+    setExpenses((prevExpense) => {
+      return [expense, ...prevExpense];
+    });
     console.log("in APP.js");
     console.log(expense);
   };
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
