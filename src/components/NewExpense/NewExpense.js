7bf82811f64f45f81c24c20930d4aed199d0160d
iftahprogramming <iftahprogramming@gmail.com>
2022-08-30 10:49:49 +0300

    Added conditional logic to NewExpense and ExpenseForm clicking add expense button calling statment and rendering the expense form , clicking cancel or save on ExpenseForm renders it back to the add expense button.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,36 @@
-import react from "react";
+import react, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setisEditing] = useState(false);
+
+
+
   const onSaveExpenseDataHandler = (enteredExepenseData) => {
     const expenseData = {
       ...enteredExepenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setisEditing(false);
   };
+
+  const startEditingHandler = (event) => {
+    setisEditing(true);
+  };
+  const stopEditingHandler = (event) => {
+    setisEditing(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+      {!isEditing && <button onClick={startEditingHandler}> Add New Expense</button>}
+      {isEditing && <ExpenseForm
+        onSaveExpenseData={onSaveExpenseDataHandler}
+        onCancel={stopEditingHandler}
+      />}
+      
     </div>
   );
 };
